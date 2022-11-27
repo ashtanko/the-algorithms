48a915d1253571670b637ceccbfe460edd4c163c
Oleksii Shtanko <shtankopro@gmail.com>
2022-11-27 18:18:56 +0200

    Add solution of Add Binary problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/AddBinary.kt
@@ -0,0 +1,79 @@
+/*
+ * MIT License
+
+ * Copyright (c) 2022 Oleksii Shtanko
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+package dev.shtanko.algorithms.leetcode
+
+import java.math.BigInteger
+
+/**
+ * 67. Add Binary
+ * Given two binary strings a and b, return their sum as a binary string.
+ * @link https://leetcode.com/problems/add-binary/
+ */
+interface AddBinaryStrategy {
+    fun perform(a: String, b: String): String
+}
+
+/**
+ * Time complexity: O(max(N,M)), where N and M are lengths of the input strings a and b.
+ * Space complexity: O(max(N,M)) to keep the answer.
+ */
+class AddBinaryBitByBitComputation : AddBinaryStrategy {
+    override fun perform(a: String, b: String): String {
+        val sb = StringBuilder()
+        var i: Int = a.length - 1
+        var j: Int = b.length - 1
+        var carry = 0
+        while (i >= 0 || j >= 0) {
+            var sum = carry
+            if (j >= 0) sum += b[j--] - '0'
+            if (i >= 0) sum += a[i--] - '0'
+            sb.append(sum % 2)
+            carry = sum / 2
+        }
+        if (carry != 0) sb.append(carry)
+        return sb.reverse().toString()
+    }
+}
+
+/**
+ * Time complexity : O(N+M), where N and M are lengths of the input strings a and b.
+ * Space complexity: O(max(N,M)) to keep the answer.
+ */
+class AddBinaryBitManipulation : AddBinaryStrategy {
+    override fun perform(a: String, b: String): String {
+        if (a.isEmpty() || b.isEmpty()) return ""
+        var x = BigInteger(a, 2)
+        var y = BigInteger(b, 2)
+        val zero = BigInteger("0", 2)
+        var carry: BigInteger
+        var answer: BigInteger
+        while (y.compareTo(zero) != 0) {
+            answer = x.xor(y)
+            carry = x.and(y).shiftLeft(1)
+            x = answer
+            y = carry
+        }
+        return x.toString(2)
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/AddBinaryTest.kt
@@ -0,0 +1,91 @@
+/*
+ * MIT License
+
+ * Copyright (c) 2022 Oleksii Shtanko
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+package dev.shtanko.algorithms.leetcode
+
+import dev.shtanko.algorithms.utils.measureTime
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+internal abstract class AddBinaryTest<out T : AddBinaryStrategy>(private val strategy: T) {
+
+    internal class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                "",
+                "",
+                "",
+            ),
+            Arguments.of(
+                "0",
+                "1",
+                "1",
+            ),
+            Arguments.of(
+                "0",
+                "0",
+                "0",
+            ),
+            Arguments.of(
+                "11",
+                "1",
+                "100",
+            ),
+            Arguments.of(
+                "1010",
+                "1011",
+                "10101",
+            ),
+            Arguments.of(
+                "10101010010010101001010010100100101",
+                "101111111111111111",
+                "10101010010010101111010010100100100",
+            ),
+            Arguments.of(
+                "10101010",
+                "11001100",
+                "101110110",
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    internal fun `add binary test`(a: String, b: String, expected: String) {
+        measureTime("Add binary a: $a b: $b") {
+            val actual = strategy.perform(a, b)
+            assertThat(actual).isEqualTo(expected)
+        }
+    }
+}
+
+internal class AddBinaryBitByBitComputationTest :
+    AddBinaryTest<AddBinaryBitByBitComputation>(AddBinaryBitByBitComputation())
+
+internal class AddBinaryBitManipulationTest :
+    AddBinaryTest<AddBinaryBitManipulation>(AddBinaryBitManipulation())
